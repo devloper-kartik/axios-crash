ce852a821cefd3af8301910ad0d52174368ccb02
devloper-kartik <112373403+devloper-kartik@users.noreply.github.com>
2022-09-11 21:25:42 +0530

    commit

--- main.js
+++ main.js
@@ -1,51 +1,134 @@
 // GET REQUEST
 function getTodos() {
-  console.log('GET Request');
+  axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log('POST Request');
+  axios.post('https://jsonplaceholder.typicode.com/todos',{
+    title: 'new task',
+    completed : false
+  })
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+  axios.put('https://jsonplaceholder.typicode.com/todos/2',{
+    title: 'done',
+    completed : true
+  })
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // DELETE REQUEST
 function removeTodo() {
-  console.log('DELETE Request');
+  axios.delete('https://jsonplaceholder.typicode.com/todos/2')
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+  axios.all([
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10'),
+    axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+  ])
+  .then(axios.spread((todo,post)=>{
+    showOutput(post);
+    console.log(todo);
+  }))
+  .catch(er=>console.log(er));
 }
 
 // CUSTOM HEADERS
-function customHeaders() {
-  console.log('Custom Headers');
+function customHeaders() {  
+  axios.post('https://jsonplaceholder.typicode.com/todos?_limit=5',{
+    title: 'new task',
+    completed : false
+  },{
+    headers:{
+      "content-type": 'application/Json',
+      Authorization: 'sometoken'
+    }
+  }
+  )
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log('Transform Response');
+  const options = {
+    method:'post',
+    url:'https://jsonplaceholder.typicode.com/todos',
+    data:{
+      title:'kartik'
+    },
+    transformResponse : axios.defaults.transformResponse.concat(data=>{
+      data.title = data.title.toUpperCase();
+      return data;
+    })
+  }
+
+  axios.get(options)
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+  axios.get('https://jsonplaceholder.typicode.com/todost')
+  .then(res=>showOutput(res))
+  .catch(err=>{
+    if(err.response){
+      alert('you got problem with because of this machine');
+    }else if(err.request){
+      alert('server is down');
+    }else{
+      alert('something is wrong');
+    }
+  });
 }
 
 // CANCEL TOKEN
 function cancelToken() {
-  console.log('Cancel Token');
+  let source = axios.CancelToken.source();
+
+  axios.get('https://jsonplaceholder.typicode.com/todos',{
+    cancelToken:source.token
+  })
+  .then(res=>showOutput(res))
+  .catch(err=>{
+    if(axios.isCancel(err)){
+      console.log('Request canceled', err.message);
+    }
+  });
+
+  if(true) source.cancel('Request canceled')
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
+axios.interceptors.request.use(
+  config=>{
+    console.log(`${config.method.toUpperCase()} request sent to 
+    ${config.url} at ${ new Date().getMinutes()}:${new Date().getSeconds()}`);
+    return config;
+  },
+  error=>{}
+);
 
 // AXIOS INSTANCES
+const instant = axios.create({
+  baseURL : 'https://jsonplaceholder.typicode.com/'
+});
+
+// instant.get('comments').then(re=>showOutput(re));
+
 
 // Show output in browser
 function showOutput(res) {
@@ -56,7 +139,7 @@ function showOutput(res) {
 
   <div class="card mt-3">
     <div class="card-header">
-      Headers
+      <h4>Headers</h4>
     </div>
     <div class="card-body">
       <pre>${JSON.stringify(res.headers, null, 2)}</pre>
@@ -65,7 +148,7 @@ function showOutput(res) {
 
   <div class="card mt-3">
     <div class="card-header">
-      Data
+     <h4> Data </h4>
     </div>
     <div class="card-body">
       <pre>${JSON.stringify(res.data, null, 2)}</pre>
